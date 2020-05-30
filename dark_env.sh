845c3f50621a836f56e6c0fd9ec541945939575c
jdh4 <52128661+jdh4@users.noreply.github.com>
2020-05-30 14:48:20 -0400

    Update dark_env.sh

--- dark_env.sh
+++ dark_env.sh
@@ -2,7 +2,7 @@
 module purge
 module load anaconda3/2020.2
 conda create --name dark-env --channel conda-forge --channel pytorch pytorch torchvision \
-cudatoolkit=10.2 matplotlib cython h5py pyfftw notebook ipdb -y
+cudatoolkit=10.2 matplotlib cython h5py pyfftw notebook -y
 conda activate dark-env
 
 module load rh/devtoolset/8 cudatoolkit/10.2 
@@ -14,6 +14,7 @@ pip install torch-spline-conv==latest+${CUDA} -f https://pytorch-geometric.com/w
 pip install torch-geometric
 
 pip install line-profiler
+pip install ipdb
 
 module load openmpi/gcc/3.1.5/64
 export MPICC=$(which mpicc)
