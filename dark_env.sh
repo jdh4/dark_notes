023e7c1f4b9feb214f085ce2957bf0ee0afe8c40
jdh4 <52128661+jdh4@users.noreply.github.com>
2020-06-01 12:53:42 -0400

    Update dark_env.sh

--- dark_env.sh
+++ dark_env.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 module purge
 module load anaconda3/2020.2
-conda create --name dark-env --channel conda-forge --channel pytorch pytorch torchvision \
-cudatoolkit=10.2 pytables matplotlib cython h5py pyfftw notebook -y
+conda create --name dark-env --channel conda-forge --channel pytorch pytorch=1.4 torchvision \
+cudatoolkit=10.1 pytables matplotlib cython h5py pyfftw notebook -y
 conda activate dark-env
 
-module load rh/devtoolset/8 cudatoolkit/10.2 
+module load rh/devtoolset/8 cudatoolkit/10.1
 CUDA=cu102
 pip install torch-scatter==latest+${CUDA} -f https://pytorch-geometric.com/whl/torch-1.4.0.html
 pip install torch-sparse==latest+${CUDA} -f https://pytorch-geometric.com/whl/torch-1.4.0.html
