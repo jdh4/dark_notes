c415ebc25910a153bd6d2966604404d652ed7278
jdh4 <52128661+jdh4@users.noreply.github.com>
2020-06-08 22:37:22 -0400

    Update dark_env.sh

--- dark_env.sh
+++ dark_env.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 module purge
 module load anaconda3/2020.2
-conda create --name dark-env-v2 --channel conda-forge --channel pytorch python=3.7 pytorch=1.5 torchvision \
-cudatoolkit=10.1 pytables matplotlib cython h5py pyfftw notebook tqdm cupy -y
-conda activate dark-env-v2
+conda create --name dark-env --channel conda-forge --channel pytorch python=3.7 pytorch=1.4 torchvision \
+cudatoolkit=10.1 pytables matplotlib cython h5py pyfftw notebook tqdm -y
+conda activate dark-env
 
 module load rh/devtoolset/8
 CUDA=cu101
-pip install torch-scatter==latest+${CUDA} -f https://pytorch-geometric.com/whl/torch-1.5.0.html
-pip install torch-sparse==latest+${CUDA} -f https://pytorch-geometric.com/whl/torch-1.5.0.html
-pip install torch-geometric
+pip install torch-scatter==latest+${CUDA} -f https://pytorch-geometric.com/whl/torch-1.4.0.html
+pip install torch-sparse==latest+${CUDA} -f https://pytorch-geometric.com/whl/torch-1.4.0.html
+pip install torch-cluster==latest+${CUDA} -f https://pytorch-geometric.com/whl/torch-1.4.0.html
+pip install torch-spline-conv==latest+${CUDA} -f https://pytorch-geometric.com/whl/torch-1.4.0.html
+wget https://github.com/rusty1s/pytorch_geometric/archive/1.4.3.tar.gz
+pip install 1.4.3.tar.gz
 
 pip install line-profiler
 pip install ipdb
